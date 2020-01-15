a57a0f6f0331780814f89b1ae49d73945b5335ee
Margarito Valencia <margaritovalenciaIII@gmail.com>
2020-01-15 14:49:13 -0600

    Dynamic chatboxes
    Made a dynamic way for different characters to have different color boxes and text. Working on image to show.

--- js/app.js
+++ js/app.js
@@ -40,22 +40,51 @@ function menu() {
 
 }
 menu();
+//NaN !=  NaN
 //Use an object to hold all conversations. Then call it and make a function to change the color of the conversation depending on the person.
-// function characters (text) {
-//     let characterChatBox = document.createElement('div');
-//     characterChatBox.className = 'characterChatBox'
-//     let
-    
-// }
+var filler = new Image();
+filler.src = "media/frame-1.png"
+
+function characters(text, characterChatColor ) {
+    let characterText = document.getElementsByClassName('characterText')[0];
+    let characterChatBox = document.createElement('div');
+    characterChatBox.className = ('row box');
+    characterText.appendChild(characterChatBox);
+    let picture = document.createElement('div');
+    picture.className = ('col-xs-4 col-sm-2');
+    characterChatBox.appendChild(picture);
+    let image = document.createElement('img');
+    image.className = ('img-responsive')
+    //image.src = mediaPath;
+    image.appendChild(picture);
+    let dialogueBox = document.createElement('div');
+    dialogueBox.className = ('col-sm-10 col-xs-8');
+    characterChatBox.appendChild(dialogueBox);
+    let dialogue = document.createElement('p');
+    dialogue.innerHTML = text;
+    dialogueBox.appendChild(dialogue);
+
+    let color = characterChatBox
+    color.style.backgroundColor = characterChatColor;
+}
+
 
 function village () {
     var hideSceneOne = document.getElementsByClassName('village')[0].style.display = 'initial';
-    let background = document.getElementsByClassName('characterAnimation')[0];
-    background.style.backgroundImage = "url('media/village8bit.png')"
-    background.style.backgroundRepeat = 'no-repeat'
-    background.style.backgroundSize = 'cover'
-
     let optionOne = document.getElementById('buttonOne');
     let optionTwo = document.getElementById('buttonTwo');
+    // optionTwo.addEventListener('click', first)
+    // optionOne.addEventListener('click', first)
+    // function first() {
+    //     if (optionOne) {
+    //         characters('Hello World', 'gray')
+    
+    //     } else if (optionTwo) {
+    //         characters('Hello WOrld', 'blue')
+    
+    //     }
+        
+    // }
+    
 
 }
